acc36b05c08a50f3316ecfda78b75beec9fb6539
Alberto-G1 <nuwarindaalbertgrande@gmail.com>
2025-06-29 22:52:20 +0300

    Added   the receptionist model and intergrated itinto the user

--- src/main/java/org/medcare/dao/UserDAO.java
+++ src/main/java/org/medcare/dao/UserDAO.java
@@ -9,7 +9,18 @@ import org.medcare.utils.HibernateUtil;
 public class UserDAO extends GenericDAO<User> {
     public UserDAO() { super(User.class); }
 
+    // This method is now used for login, so it MUST only find active users.
     public User findByUsername(String username) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            return session.createQuery("FROM User WHERE username = :username AND active = true", User.class)
+                    .setParameter("username", username)
+                    .uniqueResultOptional()
+                    .orElse(null);
+        }
+    }
+
+    // New method for admin purposes to find a user regardless of active status
+    public User findByUsernameIncludeInactive(String username) {
         try (Session session = HibernateUtil.getSessionFactory().openSession()) {
             return session.createQuery("FROM User WHERE username = :username", User.class)
                     .setParameter("username", username)
