2bbe1a315edde0d2284f389def090de923f6ca80
Alberto-G1 <nuwarindaalbertgrande@gmail.com>
2025-07-08 08:36:57 +0300

    Made chages to the Srvices to match the new DAOs

--- src/main/java/org/medcare/service/DoctorService.java
+++ src/main/java/org/medcare/service/DoctorService.java
@@ -15,6 +15,14 @@ public class DoctorService {
     private UserService userService;
 
     public void save(Doctor doctor) {
+        doctorDAO.save(doctor);
+    }
+
+    public void update(Doctor doctor) {
+        doctorDAO.update(doctor);
+    }
+
+    public void saveOrUpdate(Doctor doctor) {
         doctorDAO.saveOrUpdate(doctor);
     }
 
@@ -24,15 +32,37 @@ public class DoctorService {
         }
     }
 
+    public void reactivate(Doctor doctor) {
+        if (doctor != null && doctor.getUser() != null) {
+            userService.reactivateUser(doctor.getUser());
+        }
+    }
+
+    // Returns only active doctors
     public List<Doctor> getAll() {
         return doctorDAO.findAll();
     }
 
+    // Method to get all doctors including inactive ones
+    public List<Doctor> getAllIncludeInactive() {
+        return doctorDAO.findAllIncludeInactive();
+    }
+
+    // Returns only active doctor
     public Doctor getById(int id) {
         return doctorDAO.findById(id);
     }
 
+    // Method to get doctor by ID including inactive ones
+    public Doctor getByIdIncludeInactive(int id) {
+        return doctorDAO.findByIdIncludeInactive(id);
+    }
+
     public Doctor findByUserId(int userId) {
         return doctorDAO.findByUserId(userId);
+    }
+
+    public Doctor findByUserIdIncludeInactive(int userId) {
+        return doctorDAO.findByUserIdIncludeInactive(userId);
     }
 }
--- src/main/java/org/medcare/service/ReceptionistService.java
+++ src/main/java/org/medcare/service/ReceptionistService.java
@@ -1,4 +1,5 @@
 package org.medcare.service;
+
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
 import org.medcare.dao.ReceptionistDAO;
@@ -15,6 +16,14 @@ public class ReceptionistService {
     private UserService userService;
 
     public void save(Receptionist receptionist) {
+        receptionistDAO.save(receptionist);
+    }
+
+    public void update(Receptionist receptionist) {
+        receptionistDAO.update(receptionist);
+    }
+
+    public void saveOrUpdate(Receptionist receptionist) {
         receptionistDAO.saveOrUpdate(receptionist);
     }
 
@@ -24,15 +33,37 @@ public class ReceptionistService {
         }
     }
 
+    public void reactivate(Receptionist receptionist) {
+        if (receptionist != null && receptionist.getUser() != null) {
+            userService.reactivateUser(receptionist.getUser());
+        }
+    }
+
+    // Returns only active receptionists
     public List<Receptionist> getAll() {
         return receptionistDAO.findAll();
     }
 
+    // Method to get all receptionists including inactive ones
+    public List<Receptionist> getAllIncludeInactive() {
+        return receptionistDAO.findAllIncludeInactive();
+    }
+
+    // Returns only active receptionist
     public Receptionist getById(int id) {
         return receptionistDAO.findById(id);
     }
 
+    // Method to get receptionist by ID including inactive ones
+    public Receptionist getByIdIncludeInactive(int id) {
+        return receptionistDAO.findByIdIncludeInactive(id);
+    }
+
     public Receptionist findByUserId(int userId) {
         return receptionistDAO.findByUserId(userId);
+    }
+
+    public Receptionist findByUserIdIncludeInactive(int userId) {
+        return receptionistDAO.findByUserIdIncludeInactive(userId);
     }
 }
